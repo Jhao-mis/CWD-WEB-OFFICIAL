b80de2df0830ba16029755d8228df6583cbd68a9
GHDMarvida <dranrebmarvida@gmail.com>
2026-07-01 17:06:55 +0800

    modified:   03_bac1.php

--- 03_bac1.php
+++ 03_bac1.php
@@ -243,7 +243,7 @@
                                     </div>
 
                                     <div class="data-row">
-<<<<<<< HEAD
+<<<<<<< giHEAD
                                         <a href="assets\Files\bac\BidOpp\2026\June\CWD 29-2026.zip" class="col-date bac text-left text-decoration-none">CWD 29-2026</a>
                                         <a href="assets\Files\bac\BidOpp\2026\June\CWD 29-2026.zip" class="col-title bac text-decoration-none">Supply and Delivery of Various Chemicals for Septage Treatment  Plant</a>
                                         <div class="col-date">June 29</div>
