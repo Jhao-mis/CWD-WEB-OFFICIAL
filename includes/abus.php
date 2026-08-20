b1bf1f89d7fe661f395cc811d18df556825a530c
GHDMarvida <dranrebmarvida@gmail.com>
2026-08-20 08:53:54 +0800

    About Us Position Update TSD 08202026

--- includes/abus.php
+++ includes/abus.php
@@ -933,7 +933,7 @@
                                     </div>
                                     <div class="p-4 text-center">
                                         <h4 class="text-lg font-extrabold text-slate-800">Engr. Rolando V. Baro</h4>
-                                        <p class="text-blue-600 text-[12px] font-bold uppercase mt-1">OIC, Engineering
+                                        <p class="text-blue-600 text-[12px] font-bold uppercase mt-1">Engineering
                                             and Maintenance Division Manager</p>
                                     </div>
                                 </div>
